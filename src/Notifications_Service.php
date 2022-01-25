1972f552d8f509f7767d3d3aff42d6136213c0cc
stoyan-g <67575788+stoyan-g@users.noreply.github.com>
2022-01-25 08:58:46 +0200

    Initial commit

--- src/Notifications_Service.php
+++ src/Notifications_Service.php
@@ -2,7 +2,6 @@
 /**
  * SiteGround Notification Service
  */
-
 namespace SiteGround_Notifications;
 
 /**
@@ -10,4 +9,13 @@ namespace SiteGround_Notifications;
  */
 class Notifications_Service {
 
+    public $sg_textdomain;
+
+    public $sg_rate_option;
+
+    public $sg_ebook_attributes = array();
+
+    public function __construct( $sg_textdomain, $sg_rate_option ) {
+        // code...
+    }
 }
